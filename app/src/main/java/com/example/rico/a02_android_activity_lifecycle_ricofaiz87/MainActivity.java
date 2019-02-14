366598219f8769fb2a7afc9e581f30b841b1a533
ricofaiz87 <rico.faiz19@gmail.com>
2019-02-14 10:02:22 +0700

    android activity lifecycle

---
+++ app/src/main/java/com/example/rico/a02_android_activity_lifecycle_ricofaiz87/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rico.a02_android_activity_lifecycle_ricofaiz87;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
